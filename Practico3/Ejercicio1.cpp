a2fc9d2b85b984d83a28e59b1ff7db62ff3849d1
Manuel Muniz <m50030709@china.huawei.com>
2023-08-18 14:19:06 -0300

    Mas ejercicios

--- Practico3/Ejercicio1.cpp
+++ Practico3/Ejercicio1.cpp
@@ -2,37 +2,42 @@
 
 typedef unsigned int uint;
 
-void insertarOrdenado(int *A, uint n, int e){
-    if((n=0)||e>=A[n]){
-        A[n+1]=e;
-    }else{
-        A[n+1]=A[n];
-        insertarOrdenado(A, n-1, e);
-    }
-}
-
 /* Ordena A[1.. n] de manera creciente. */
-void ordenar(int *A, uint n){
-    if(n>1){
-        ordenar(A, n-1);
-        insertarOrdenado(A, n-1, A[n]);
+void ordenar(int *A, uint n)
+{
+    if (n > 1)
+    {
+        ordenar(A, n - 1);
+        insertarOrdenado(A, n - 1, A[n]);
     }
 }
 
+void insertarOrdenado(int *A, uint n, int e)
+{
+    if (n == 0 || e >= A[n])
+    {
+        A[n + 1] = e;
+    }
+    else
+    {
+    }
+}
 
-
-int main(int argc, char const *argv[]){
-    int array[]={3,6,21,6,1};
-    for (int i = 0; i < 5; i++){
+int main(int argc, char const *argv[])
+{
+    int array[] = {3, 6, 21, 6, 1};
+    for (int i = 0; i < 5; i++)
+    {
         printf("%d", array[i]);
     }
 
     printf("\n");
     ordenar(array, 5);
 
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < 5; i++)
+    {
         printf("%d", array[i]);
     }
-    
+
     return 0;
 }
