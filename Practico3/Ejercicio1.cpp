b2d24a36101156fc04a94a69598e5bbcec4105c3
Manuel Muniz <m50030709@china.huawei.com>
2023-08-21 14:24:27 -0300

    Mas ejercicios

--- Practico3/Ejercicio1.cpp
+++ Practico3/Ejercicio1.cpp
@@ -2,6 +2,37 @@
 
 typedef unsigned int uint;
 
+/* Inserta e en A [1..n+1] de manera ordenada.
+Precondición: n >=0. Si n >=1 = > A [1.. n] est á ordenado de manera
+creciente. A[n+1] es indeterminado.
+Postcondición: A [1..n +1] queda ordenado de manera creciente.
+
+Recursion de cola: La llamada recursiva es la ultima instruccion que se ejecuta.*/
+
+void insertarOrdenado(int *A, uint n, int e)
+{
+    if (n == 0 || e >= A[n])
+    {
+        A[n + 1] = e;
+    }
+    else
+    {
+        A[n + 1] = A[n];
+        insertarOrdenado(A, n - 1, e);
+    }
+}
+
+// Version iterativa de insertarOrdenado
+void insertarOrdenadoV2(int *A, uint n, int e)
+{
+    while (n == 0 || e >= A[n])
+    {
+        A[n + 1] = A[n];
+        n--;
+    }
+    A[n + 1] = e;
+}
+
 /* Ordena A[1.. n] de manera creciente. */
 void ordenar(int *A, uint n)
 {
@@ -12,27 +43,16 @@ void ordenar(int *A, uint n)
     }
 }
 
-void insertarOrdenado(int *A, uint n, int e)
-{
-    if (n == 0 || e >= A[n])
-    {
-        A[n + 1] = e;
-    }
-    else
-    {
-    }
-}
-
 int main(int argc, char const *argv[])
 {
-    int array[] = {3, 6, 21, 6, 1};
+    int array[] = {0, 7, 8, 6, 1};
     for (int i = 0; i < 5; i++)
     {
         printf("%d", array[i]);
     }
 
     printf("\n");
-    ordenar(array, 5);
+    ordenar(array, 4);
 
     for (int i = 0; i < 5; i++)
     {
